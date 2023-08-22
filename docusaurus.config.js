bb16bf30c26c4142598458fbd6dddcc1f3f201c2
kenpaicat <133065911+kenpaicat@users.noreply.github.com>
2023-08-22 21:58:30 +0200

    add OpenAPI schema (incomplete)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,62 +1,81 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Laguna docs',
+  title: "Laguna docs",
   // tagline: 'Dinosaurs are cool',
-  favicon: 'img/logo.png',
+  favicon: "img/logo.png",
 
   // Set the production url of your site here
-  url: 'https://sloveniaengineering.github.io',
+  url: "https://sloveniaengineering.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/laguna-docs/',
+  baseUrl: "/laguna-docs/",
   trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'SloveniaEngineering', // Usually your GitHub org/user name.
-  projectName: 'laguna-docs', // Usually your repo name.
-  deploymentBranch: 'deploy',
+  organizationName: "SloveniaEngineering", // Usually your GitHub org/user name.
+  projectName: "laguna-docs", // Usually your repo name.
+  deploymentBranch: "deploy",
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'sl'],
+    defaultLocale: "en",
+    locales: ["en", "sl"],
   },
 
   presets: [
+    /*
     [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
+      "classic",
+      // @type {import('@docusaurus/preset-classic').Options}
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
+    ],
+    */
+    [
+      "docusaurus-preset-openapi",
+      /** @type {import('docusaurus-preset-openapi').Options} */
+      {
+        api: {
+          path: "api/schema.yaml",
+          routeBasePath: "/api",
+        },
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
     ],
   ],
 
@@ -64,39 +83,48 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/logo.png',
+      image: "img/logo.png",
       navbar: {
-        title: 'Laguna docs',
+        title: "Laguna docs",
         logo: {
-          alt: 'Logo',
-          src: 'img/logo.png',
+          alt: "Logo",
+          src: "img/logo.png",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            to: '/api',
+            label: 'API',
             position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/SloveniaEngineering/laguna-docs',
-            label: 'GitHub',
-            position: 'right',
           },
           {
-            type: 'localeDropdown',
-            position: 'right',
+            type: "docSidebar",
+            sidebarId: "apiDocsSidebar",
+            position: "left",
+            label: "Docs",
+          },
+          {
+            to: "/blog",
+            label: "Blog",
+            position: "left",
+          },
+          {
+            href: "https://github.com/SloveniaEngineering/laguna-docs",
+            label: "GitHub",
+            position: "right",
+          },
+          {
+            type: "localeDropdown",
+            position: "right",
             items: [
               {
-                to: '/',
-                label: 'English',
+                to: "/",
+                label: "English",
               },
               {
-                to: '/sl/',
-                label: 'Slovenščina',
+                to: "/sl/",
+                label: "Slovenščina",
               },
-            ]
+            ],
           },
         ],
       },
